29d242ae7dd249268fdacaf7a56ef14310d89321
Johannes Fritz <fritz.johannes@gmail.com>
2019-12-03 23:21:35 +0100

    adjusted to gta25_setup()

--- code/Sectoral chapters panel 1/code/0 Data prep.R
+++ code/Sectoral chapters panel 1/code/0 Data prep.R
@@ -7,12 +7,15 @@ library(tidyverse)
 
 gta_setwd()
 source('0 report production/GTA 25/help files/Producer console.R')
-source('0 report production/GTA 25/help files/GTA 25 cutoff and definitions.R')
 
-this.chapter=c(paste0("Sectoral chapters - Sector ",paste0(sectors)))
 
-wdpath = "0 dev/gta-25-pb/code/Sectoral chapters panel 1/"
-data.path = paste0(wdpath,"data/")
+directories=gta25_setup(internal.name="Sectoral chapters panel 1",
+                        in.dev=F,
+                        author=NULL,
+                        wipe.data=T,
+                        wipe.figs=T)
+
+data.path = directories$data.path
 
 run.calc=T
 
