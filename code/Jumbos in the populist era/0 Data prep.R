80f8a6563ace54621c1c57a07f19c34602df6c91
Lukaszuk <piotr.lukaszuk@student.unisg.ch>
2019-12-11 13:47:18 +0100

    Jumbo chart and coverage calculation added as by SE request

---
+++ code/Jumbos in the populist era/0 Data prep.R
@@ -0,0 +1,58 @@
+rm(list=ls())
+
+## Req: Create bar chart with jumbos introduced in populist era
+
+library(gtalibrary)
+library(stringr)
+library(tidyverse)
+library(plyr)
+library(data.table)
+
+gta_setwd()
+source('0 report production/GTA 25/help files/Producer console.R')
+chapter.folders=gta25_setup(internal.name = 'Jumbos in the populist era',
+                            in.dev = F,
+                            author=NULL,
+                            wipe.data = T,
+                            wipe.figs = T)
+data.path=chapter.folders$data.path
+figure.path=chapter.folders$figure.path
+
+jumbo.data = xlsx::read.xlsx2(file = "0 report production/GTA 25/prep pre report/trade per intervention_jumbos checked.xlsx", sheetName = "populist era")
+jumbo.data$trade.value = as.numeric(as.character(jumbo.data$trade.value))
+jumbo.data = subset(jumbo.data, is.na(trade.value)==F)
+jumbo.data$date.implemented = as.Date(as.numeric(as.character(jumbo.data$date.implemented)), origin = "1970-01-01")
+jumbo.data$date.implemented = as.character(jumbo.data$date.implemented)
+jumbo.data$order = nrow(jumbo.data):1
+
+save(jumbo.data, file = "0 report production/GTA 25/prep pre report/jumbo.data.Rdata")
+
+### SE: Calculate amount of trade affected by these jumbos overall, US & China
+
+gta_trade_coverage(gta.evaluation = c("Red", "Amber"),
+                   intervention.ids = jumbo.data$intervention.id,
+                   keep.interventions = T,
+                   affected.flows = c("inward", "outward","outward subsidy"),
+                   coverage.period=c(2017,2019),
+                   trade.data = 2016,
+                   trade.statistic = "value")
+#Overall: 2.64E+12	3.69E+12	3.04E+12
+
+gta_trade_coverage(gta.evaluation = c("Red", "Amber"),
+                   intervention.ids = subset(jumbo.data, implementing.jurisdiction=="United States of America")$intervention.id,
+                   keep.interventions = T,
+                   affected.flows = c("inward", "outward","outward subsidy"),
+                   coverage.period=c(2017,2019),
+                   trade.data = 2016,
+                   trade.statistic = "value")
+#USA: 	63219931247	294237318540	539788298278
+
+gta_trade_coverage(gta.evaluation = c("Red", "Amber"),
+                   intervention.ids = subset(jumbo.data, implementing.jurisdiction=="China")$intervention.id,
+                   keep.interventions = T,
+                   affected.flows = c("inward", "outward","outward subsidy"),
+                   coverage.period=c(2017,2019),
+                   trade.data = 2016,
+                   trade.statistic = "value")
+#China: 433937520	370192776164	8.37204e+11
+
