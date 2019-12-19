46033a4f709f03995166808565051ee1f5304334
Johannes Fritz <fritz.johannes@gmail.com>
2019-12-19 13:32:00 +0100

    changed country groups

--- code/Country groups targeted by destination markets/code/0 Data prep.R
+++ code/Country groups targeted by destination markets/code/0 Data prep.R
@@ -21,15 +21,16 @@ data.path = directories$data.path
 run.calc=T
 
 trade.data.year = "base"
-
+period <- c(as.character(as.Date(break.date)+1), cutoff)
 # Figure 1 data prep ------------------------------------------------------
 
 # Graph 1: Y-axis shows the share of bilateral exports of LDCs/African Union
 # facing (1) harmful measures implemented by destination markets and (2) reforming/liberalising
 # measures implemented by destination market. The X-axis shows the destination markets. (see attachement)
+bris.members=setdiff(brics.members,156)
 
-dest.markets <- list(c(156), c(eu.members), c(840), c(g7.members), c(brics.members), c(g20.members))
-dest.markets.names <- c("China","EU","USA","G7","BRICS","G20")
+dest.markets <- list(c(156), c(eu.members), c(840), c(bris.members), c(g20.members))
+dest.markets.names <- c("China","EU","USA","BRIS","G20")
 targeted <- list(c(ldc.countries),c(au.members),c(wb.low.mid.ctry),c(wb.upp.mid.ctry))
 targeted.names <- c("LDCs","African Union","Lower middle income countries","Upper middle income countries")
 
@@ -47,6 +48,7 @@ if (run.calc) {
                          keep.importers = T,
                          implementer.role = "importer",
                          coverage.period = c(2019,2019),
+                         implementation.period = period,
                          trade.data = trade.data.year)
       
       dest.markets.cov <- rbind(dest.markets.cov, data.frame(importer = dest.markets.names[dst],
@@ -63,6 +65,7 @@ if (run.calc) {
                          keep.importers = T,
                          implementer.role = "importer",
                          coverage.period = c(2019,2019),
+                         implementation.period = period,
                          trade.data = trade.data.year)
       
       dest.markets.cov <- rbind(dest.markets.cov, data.frame(importer = dest.markets.names[dst],
